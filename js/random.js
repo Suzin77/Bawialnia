278beada2f2dbe8e348a666ff7600192f24ccf82
Patrycjusz Sosnowski <patrycjusz.sosnowski@gmail.com>
2019-03-06 20:23:43 +0100

    prototyping features , progress bars , eventlisteners etc

--- js/random.js
+++ js/random.js
@@ -10,7 +10,90 @@ $(".document").ready(function(){
             randomImgList[i].setAttribute("src", "img/"+random(1,20)+".png");
         }        
     });
+
+
 });
+
+const itemAttack = {
+  '1': 20,
+  '11' : 10,
+  '14': 5
+}
+
+const startConfig = {
+  cutTime : 1,
+  digTime : 4
+  
+}
+
+function move(time) {
+    let parentElement = $(this)[0].parentElement;
+    var elem = parentElement.querySelector("#myBar");
+    let procentValue  = parentElement.querySelector(".actualProgrssValue");
+    let actualScoreElement = parentElement.querySelector(".totalScore");
+    let actualScore = actualScoreElement.textContent;
+    var width = 1;
+    let actualContext = this;
+    
+    actualContext.disabled = true;
+    var id = setInterval(frame, time*1000/100);
+    function frame() {
+      if (width >= 100) {
+        actualScoreElement.textContent = actualScore*1 + 1;
+        actualContext.disabled = false;
+        clearInterval(id);
+      } else {
+        width++; 
+        elem.style.width = width + '%';
+        procentValue.innerHTML = width*1 + ' %'; 
+      }
+     
+    }
+  }
+
+function collectFunction(){
+  let resourceName = this.id;
+  resourceName = resourceName.charAt(0).toUpperCase() + resourceName.slice(1);
+  let parentElement = $(this)[0].parentElement;
+  console.log(parentElement.className);
+  let actualScoreElement = parentElement.querySelector(".totalScore");
+  let actualScore = actualScoreElement.textContent;
+  let actualInventory = document.querySelector("#inventory"+resourceName).textContent;
+  document.querySelector("#inventory"+resourceName).textContent = actualScore*1 + actualInventory*1;
+  actualScoreElement.textContent = 0;
+  //document.querySelector(".inventoryWood").innerHTML = `<p>Wood <span>${actualScore}</span></p>`;
+}
+
+function attack(){
+  let imageName = $(this).attr("src");
+  let imageNumber = imageName.replace(/\D/g,'');
+  //console.log(itemAttack[13]);
+  if(itemAttack[imageNumber]){
+    let actualHP = document.querySelector("#monsterBar").textContent;
+    document.querySelector("#monsterBar").textContent =actualHP -  itemAttack[imageNumber]; 
+  }      
+  let randomImgList = document.querySelectorAll(".randomImg");
+    console.log(randomImgList);
+    for(let i = 0; i<randomImgList.length; i++){
+        randomImgList[i].setAttribute("src", "img/"+random(1,20)+".png");
+    }        
+    
+
+}
+$(document).on("click", ".collect", collectFunction);
+$(document).on("click", ".randomImg", attack);
+$(document).on("click", ".work",function (){move.call(this, startConfig.cutTime)}); 
+
+
+
+//document.querySelector("#cut").addEventListener("click", function (){move.call(this, startConfig.cutTime)}, false);
+//document.querySelector("#dig").addEventListener("click", function (){move.call(this, startConfig.digTime)}, false);
+//document.querySelector("#forge").addEventListener("click", function (){move.call(this, 500)}, false);
+
+//document.querySelector(".collect").addEventListener("click", collectFunction, false);
+
+//$("#progressbar").progressbar({value: 57});
+
 
 
 
